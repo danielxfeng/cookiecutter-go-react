b8f7b8b8a81d335d72422a6529313b2d0e3f1be9
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-04-20 23:45:46 +0300

    feat: update ESLint configuration and Prettier ignore rules, enhance precommit script

--- {{ cookiecutter.project_slug }}/apps/frontend-start/eslint.config.js
+++ {{ cookiecutter.project_slug }}/apps/frontend-start/eslint.config.js
@@ -3,4 +3,7 @@
 import { tanstackConfig } from '@tanstack/eslint-config';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default defineConfig([globalIgnores(['dist', 'src/components/ui/**']), ...tanstackConfig]);
+export default defineConfig([
+  globalIgnores(['dist', 'src/components/ui/**', 'src/routeTree.gen.ts']),
+  ...tanstackConfig,
+]);
